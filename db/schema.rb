0f07cfbc2769a555c89227c8ee3ec82fd36dde31
chobichobi62 <ono.t@ymail.ne.jp>
2024-04-11 19:16:22 +0900

    デプロイ　修正2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_10_074622) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_09_133211) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_10_074622) do
     t.bigint "prototype_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
+    t.text "text"
     t.index ["prototype_id"], name: "index_comments_on_prototype_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
